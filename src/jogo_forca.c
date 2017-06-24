b9a1f20f52d04407fca21769a9125e66e71bbb66
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-06-24 01:40:55 -0300

    Adicionando função de jogar

--- src/jogo_forca.c
+++ src/jogo_forca.c
@@ -21,6 +21,60 @@ typedef struct {
     char *arquivo_palavra;
 } Jogo;
 
+bool existe_letra_na_palavra( char carac, char *palavra );
+bool existe_letra_nas_erradas( char carac, char *letras_erradas );
+
+int comparar_mascara_palavra( char *mascara, char *palavra );
+void substituir_letra_na_mascara( char carac, char *palavra, char *mascara );
+void init_mascarara( char *mascara, char *palavra, unsigned int tamanho_palavra );
+void finalizar( Jogo *jogo );
+
+int jogar( char *arquivo_palavra, char *arquivo_dica ) {
+    Jogo jogo;
+
+    char tmp[2];
+
+    char *palavra, *mascara, *dica, *letras_erradas;
+    char carac;
+
+    init_configuracoes( &jogo, arquivo_palavra, arquivo_dica );
+
+    palavra = string_get_text( jogo.palavra.palavra );
+    mascara = string_get_text( jogo.mascara );
+    dica = string_get_text( jogo.palavra.dica.dica );
+    letras_erradas = string_get_text( jogo.letras_erradas );
+
+    do {
+        limpar_tela();
+
+        exibir_jogo( jogo.quantidade_erros, dica, mascara, letras_erradas );
+
+        printf( "\nDigite uma letra:" );
+        scanf( " %c", &carac );
+
+        if( !existe_letra_na_palavra( carac, palavra ) ) {
+            if( !existe_letra_nas_erradas( carac, letras_erradas ) ) {
+                jogo.quantidade_erros++;
+                sprintf( tmp, "%c", carac );
+                string_concat_char_array( jogo.letras_erradas, tmp );
+            }
+        } else {
+            substituir_letra_na_mascara( carac, palavra, mascara );
+        }
+
+        if( !comparar_mascara_palavra( mascara, palavra ) ) {
+            jogo.ganhou = true;
+        }
+    } while( !jogo.ganhou && jogo.quantidade_erros < MAX_ERRO );
+
+    limpar_tela();
+    exibir_resultado_e_palavra( palavra, jogo.ganhou );
+
+    finalizar( &jogo );
+
+    return !jogo.ganhou;
+}
+
 void init_configuracoes( Jogo *jogo, char *arquivo_palavra, char *arquivo_dica ) {
     jogo->quantidade_erros = 0;
     jogo->ganhou = false;
