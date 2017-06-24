3a5cbd22f5c041e860d62e3b90f2512c92c1ade0
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-06-24 01:35:05 -0300

    Criando o arquivo e adicionando a função de inicialização

---
+++ src/jogo_forca.c
@@ -0,0 +1,37 @@
+#include "jogo_forca.h"
+#include "interacao_arquivo.h"
+#include "viewer.h"
+#include <dstring.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_ERRO 6
+
+void init_configuracoes( Jogo *jogo, char *arquivo_palavra, char *arquivo_dica ) {
+    jogo->quantidade_erros = 0;
+    jogo->ganhou = false;
+
+    jogo->palavra = sortear_palavra( arquivo_palavra, arquivo_dica );
+
+    jogo->letras_erradas = string_new();
+    jogo->mascara = string_new_with_size( string_get_length( jogo->palavra.palavra ) );
+
+    init_mascarara( string_get_text( jogo->mascara ),
+                    string_get_text( jogo->palavra.palavra ),
+                    string_get_length( jogo->palavra.palavra ) );
+}
+
+void init_mascarara( char *mascara, char *palavra, unsigned int tamanho_palavra ) {
+    unsigned int i;
+
+    for( i = 0; i < tamanho_palavra; i++ ) {
+        if( palavra[i] == '_' || palavra[i] == '-' || palavra[i] == ' ' ) {
+            palavra[i] = '-';
+            mascara[i] = '-';
+        } else {
+            mascara[i] = '_';
+        }
+    }
+    mascara[i] = '\0';
+}
