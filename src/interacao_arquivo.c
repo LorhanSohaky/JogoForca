957614636bef9e6514e135e70f589ae357086fd7
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-06-24 01:26:00 -0300

    Adicionando função para pegar o total de linhas do arquivo

---
+++ src/interacao_arquivo.c
@@ -0,0 +1,25 @@
+#include "interacao_arquivo.h"
+#include <dstring.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+unsigned int pegar_total_linhas( char *arquivo_palavra ) {
+    FILE *arquivo;
+    unsigned int quantidade_linhas = 0;
+
+    arquivo = fopen( arquivo_palavra, "r" );
+    if( !arquivo ) {
+        return -1;
+    }
+
+    while( EOF != fscanf( arquivo, "%*[^\n]" ) ) {
+        fscanf( arquivo, "%*c" );
+        quantidade_linhas++;
+    }
+
+    fclose( arquivo );
+
+    return quantidade_linhas;
+}
+
