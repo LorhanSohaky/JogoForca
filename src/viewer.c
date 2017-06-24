5943d170c1d0be176ce3616caec70296711fb80f
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-06-24 01:19:21 -0300

    Adicionando função de exibir o boneco

--- src/viewer.c
+++ src/viewer.c
@@ -2,6 +2,118 @@
 #include <stdio.h>
 #include <string.h>
 
+void exibir_boneco( unsigned int quantidade_erros ) {
+    switch( quantidade_erros ) {
+        case 0:
+            printf( "┏━━━━━━━━━━━━━┓\n" );
+            printf( "┃             ┇\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            break;
+        case 1:
+            printf( "┏━━━━━━━━━━━━━┓\n" );
+            printf( "┃             ┇\n" );
+            printf( "┃            ***\n" );
+            printf( "┃           *   *\n" );
+            printf( "┃            ***\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            break;
+        case 2:
+            printf( "┏━━━━━━━━━━━━━┓\n" );
+            printf( "┃             ┇\n" );
+            printf( "┃            ***\n" );
+            printf( "┃           *   *\n" );
+            printf( "┃            ***\n" );
+            printf( "┃             │\n" );
+            printf( "┃             │\n" );
+            printf( "┃             │\n" );
+            printf( "┃             │\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            break;
+        case 3:
+            printf( "┏━━━━━━━━━━━━━┓\n" );
+            printf( "┃             ┇\n" );
+            printf( "┃            ***\n" );
+            printf( "┃           *   *\n" );
+            printf( "┃            ***\n" );
+            printf( "┃             │\n" );
+            printf( "┃            /│\n" );
+            printf( "┃           / │\n" );
+            printf( "┃             │\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            break;
+        case 4:
+            printf( "┏━━━━━━━━━━━━━┓\n" );
+            printf( "┃             ┇\n" );
+            printf( "┃            ***\n" );
+            printf( "┃           *   *\n" );
+            printf( "┃            ***\n" );
+            printf( "┃             │\n" );
+            printf( "┃            /│\\\n" );
+            printf( "┃           / │ \\\n" );
+            printf( "┃             │\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            break;
+        case 5:
+            printf( "┏━━━━━━━━━━━━━┓\n" );
+            printf( "┃             ┇\n" );
+            printf( "┃            ***\n" );
+            printf( "┃           *   *\n" );
+            printf( "┃            ***\n" );
+            printf( "┃             │\n" );
+            printf( "┃            /│\\\n" );
+            printf( "┃           / │ \\\n" );
+            printf( "┃             │\n" );
+            printf( "┃            /\n" );
+            printf( "┃           /\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            break;
+        case 6:
+            printf( "┏━━━━━━━━━━━━━┓\n" );
+            printf( "┃             ┇\n" );
+            printf( "┃            ***\n" );
+            printf( "┃           *   *\n" );
+            printf( "┃            ***\n" );
+            printf( "┃             │\n" );
+            printf( "┃            /│\\\n" );
+            printf( "┃           / │ \\\n" );
+            printf( "┃             │\n" );
+            printf( "┃            / \\\n" );
+            printf( "┃           /   \\\n" );
+            printf( "┃\n" );
+            printf( "┃\n" );
+            break;
+        default:
+            printf( "\n" );
+    }
+}
+
 void exibir_resultado_e_palavra( char *palavra, bool ganhou ) {
     printf( "Palavra: %s\n", palavra );
     if( ganhou ) {
