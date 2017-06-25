1a7894141e73e522c23ce00bbc337538398fbb94
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-06-25 20:31:37 -0300

    Verificação dos argumentos

--- src/main.c
+++ src/main.c
@@ -2,5 +2,11 @@
 #include <stdio.h>
 
 int main( int argc, char *argv[] ) {
-    return jogar( argv[1], argv[2] );
+    if( argc < 3 ) {
+        printf( "Argumentos invalidos\n" );
+        printf( "Voce deve passar o arquivo com as palavras e o arquivo com as dicas\n" );
+        return 1;
+    } else {
+        return jogar( argv[1], argv[2] );
+    }
 }
